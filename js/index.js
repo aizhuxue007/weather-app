9459e00b1f2cdeb43e68989235bfefcaa11083ce
aizhuxue007 <aizhuprogram@gmail.com>
2023-09-26 21:47:33 -0400

    starter files and basic pseudocode

---
+++ js/index.js
@@ -0,0 +1,8 @@
+// add click listener to check weather btn that calls function run
+
+// run
+    // get cityInput value
+    // fetch city weather through api
+    // render the 7 day weather to DOM
+    // if today is raining -> send sms to get raincoat
+    // if today is snowing -> send sms to wear layers and wear boots
