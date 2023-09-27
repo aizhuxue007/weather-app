d424cadb2c551fe2076afcc734b38d56b270a37e
aizhuxue007 <aizhuprogram@gmail.com>
2023-09-27 05:42:28 -0400

    added openweather url for api

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@
 // run
     // get cityInput value
     // fetch city weather through api
+        // https://openweathermap.org/api
     // render the 7 day weather to DOM
     // if today is raining -> send sms to get raincoat
     // if today is snowing -> send sms to wear layers and wear boots
